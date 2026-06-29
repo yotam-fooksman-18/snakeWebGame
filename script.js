0f14336e498cc9d952ed1fa5e412c3918b8fcffa
yotam-fooksman-18 <148906732+yotam-fooksman-18@users.noreply.github.com>
2026-06-29 13:52:43 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,16 +1,18 @@
 
-function inputU(){
-    let userInput = prompt("enter number ");
-        if ((userInput*userInput) % 2 ==0){
-            document.writeln("<div class =header>" + userInput*userInput+"</div>");
-        }
-    alert("Done");
+const grid = document.getElementById('grid');
+let squares = [];
+let currentSnake = [2,1,0];
+let diretion =1 ;
+let appleIndex =1;
+let score =0;
+let timerId=0;
+let intervalTime = 200;
+function creatBoard(){
+    for(let i =0;i<400;i++){
+    const squre = document.createElement('div');
+    // const squareChild[];
+    grid.appendChild(square);
+    squares.push(square);
 }
-function inputU2(){
-    let userInput = prompt("enter number ");
-       let x = Math.sqrt(userInput)
-            document.writeln("<div class =header>" + x+"</div>");
-
-    alert("Done");
 }
-inputU()
+creatBoard();
