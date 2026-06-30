3fab5bfb61f3e8e51c3a5dab18d52cb96d343847
yotam-fooksman-18 <148906732+yotam-fooksman-18@users.noreply.github.com>
2026-06-30 10:22:44 +0300

    Update script.js

--- script.js
+++ script.js
@@ -16,3 +16,34 @@ function creatBoard(){
 }
 }
 creatBoard();
+function startGame(){
+    currentSnake.forEach(index=> squares[index].classLis.remove('snake'));
+    squares[appleIndex].classList.remove('apple');
+    clearInterval(TimerId);
+}
+function move(){
+    const hitBottom=(currentSnake[0]+20 >=400 && direction ===20);
+    const hitTop=(currentSnake[0]-20 <0 && direction ===-20);
+    const hitRight=(currentSnake[0]%20 ===19 && direction ===1);
+    const hitLeft=(currentSnake[0]% 20 ===0 && direction ===-1);
+    const hitSelf=squares[currnetSnake[0]+direction]?.classList.contains('snake');
+}
+function generateApple(){
+    do{
+        appleIndex =Math.floor(Math.random()* squares.length);
+    }while(squars[appleIndex].classList.contains('snake'));
+ squares[appleIndex].classList.add('apple');
+}
+function changeDir(newDir){
+if (direction+newDir !=0){
+    direction=newDir;
+}
+}
+document.addEventListener('keydown',(e)=>{
+
+    if (e.key === 'ArrowUp') changeDir(-20);
+    if (e.key === 'ArrowDown') changeDir(20);
+    if (e.key === 'ArrowLeft') changeDir(-1);
+    if (e.key === 'ArrowRight') changeDir(1);
+    
+});
